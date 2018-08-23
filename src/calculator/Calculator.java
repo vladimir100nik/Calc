f591535967b5d8741f0cc7b1a2cd97a5fa7e27c8
Vladimir <vladimir100nik@gmail.com>
2018-08-23 20:28:57 +0300

    mesage

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.nio.file.spi.FileSystemProvider;
+
 /**
  * @author Dimitrijs Fedotovs <a href="http://www.bug.guru">www.bug.guru</a>
  * @version 1.0
@@ -8,7 +10,60 @@ package calculator;
 class Calculator {
 
     String calculate(String[] expression) {
-        return "0";
+        System.out.println(expression[0]);
+        System.out.println(expression[1]);
+        System.out.println(expression[2]);
+        System.out.println(expression[3]);
+        System.out.println(expression[4]);
+        double a = Double.parseDouble(expression[0]);
+        double b = Double.parseDouble(expression[2]);
+        if (expression.length < 4) (
+
+            return String.valueOf(result);
+        )
+
+    }
+        double result;
+
+        switch (expression[1]) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+            default:
+                return "Error";
+
+        }
+        double c = Double.parseDouble(expression[4]);
+        switch (expression[3]) {
+            case "+":
+                result = result + c;
+                break;
+            case "-":
+                result = result - c;
+                break;
+            case "*":
+                result = result * c;
+                break;
+            case "/":
+                result = result / c;
+                break;
+            default:
+                return "Error";
+
+        }
+
+        return String.valueOf(result);
+
+
     }
 
 }
