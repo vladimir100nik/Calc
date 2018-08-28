7be909942491ee6ee477f82ab7722031aa24284a
Vladimir <vladimir100nik@gmail.com>
2018-08-28 18:46:16 +0300

    28.09 - 2

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -10,77 +10,27 @@ import java.nio.file.spi.FileSystemProvider;
 class Calculator {
 
     String calculate(String[] expression) {
-        System.out.println(expression[0]);
-        System.out.println(expression[1]);
-        System.out.println(expression[2]);
-        //System.out.println(expression[3]);
-        //System.out.println(expression[4]);
-        double a = Double.parseDouble(expression[0]);
-        double b = Double.parseDouble(expression[2]);
+        double result = Double.parseDouble((expression[0]));
+        for (int i = 1; i < expression.length; i += 2) {
 
-
-        double result;
-
-        switch (expression[1]) {
-            case "+":
-                result = a + b;
-                break;
-            case "-":
-                result = a - b;
-                break;
-            case "*":
-                result = a * b;
-                break;
-            case "/":
-                result = a / b;
-                break;
-            default:
-                return "Error";
+            String op = expression[i];
+            double b = Double.parseDouble(expression[i + 1]);
+            switch (op) {
+                case "+":
+                    result += b;
+                    break;
+                case "-":
+                    result -= b;
+                    break;
+                default:
+                    return "Error";
+            }
         }
 
-        if (expression.length < 4) {
-            return String.valueOf(result);
-        }
-
-        double c = Double.parseDouble(expression[4]);
-
-        if (expression[1].equals("*") && expression[3].equals("+")) {
-            result = a * b + c;
-        } else if (expression[1].equals("*") && expression[3].equals("-")) {
-            result = a * b - c;
-        } else if (expression[1].equals("/") && expression[3].equals("+")) {
-            result = a / b + c;
-        } else if (expression[1].equals("/") && expression[3].equals("-")) {
-            result = a / b - c;
-        } else if (expression[1].equals("+") && expression[3].equals("*")) {
-            result = c * b + a;
-        } else if (expression[1].equals("+") && expression[3].equals("/")) {
-            result = c / b + a;
-        } else if (expression[1].equals("-") && expression[3].equals("*")) {
-            result = a - (c * b);
-        } else if (expression[1].equals("-") && expression[3].equals("/")) {
-            result = a - (b / c);
-        }
-        /*switch (expression[3]) {
-            case "+":
-                result = result + c;
-                break;
-            case "-":
-                result = result - c;
-                break;
-            case "*":
-                result = result * c;
-                break;
-            case "/":
-                result = result / c;
-                break;
-            default:
-                return "Error";
-        }*/
 
         return String.valueOf(result);
 
-
     }
+
 
 }
