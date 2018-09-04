570c6bd17d57e5194efbdb712d9ab373bcfcdbe3
Vladimir <vladimir100nik@gmail.com>
2018-09-04 18:39:49 +0300

    calc-4.09

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,6 +1,9 @@
 package calculator;
 
 import java.nio.file.spi.FileSystemProvider;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Dimitrijs Fedotovs <a href="http://www.bug.guru">www.bug.guru</a>
@@ -10,11 +13,35 @@ import java.nio.file.spi.FileSystemProvider;
 class Calculator {
 
     String calculate(String[] expression) {
-        double result = Double.parseDouble((expression[0]));
-        for (int i = 1; i < expression.length; i += 2) {
 
+        List<String> tmp = new ArrayList<>();
+        double a = Double.parseDouble((expression[0]));
+        for (int i = 1; i < expression.length; i += 2) {
             String op = expression[i];
             double b = Double.parseDouble(expression[i + 1]);
+            switch (op) {
+                case "+":
+                case "-":
+                    tmp.add(String.valueOf(a));
+                    tmp.add(op);
+                    a = b;
+                    break;
+                case "*":
+                    a *= b;
+                    break;
+                case "/":
+                    a /= b;
+                    break;
+            }
+
+        }
+        tmp.add(String.valueOf(a));
+        System.out.println(tmp);
+
+        double result = Double.parseDouble(tmp.get(0));
+        for (int i = 1; i < tmp.size(); i += 2) {
+            String op = tmp.get(i);
+            double b = Double.parseDouble(tmp.get(i+1));
             switch (op) {
                 case "+":
                     result += b;
